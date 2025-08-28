3fc6a2c402aaf4e3806703fd7b0aaf5628dd43d3
Quark3e <b.erkhme@gmail.com>
2025-08-28 16:45:50 +0200

    cum

--- gtfs-realtime_projects/scan_directory/src/main_subprocess.cpp
+++ gtfs-realtime_projects/scan_directory/src/main_subprocess.cpp
@@ -20,7 +20,7 @@ StopID_refrSorted subProcess_loadFile__stop_times(
     std::ifstream file__returVecRef_temp("returVecRef_temp.csv");
     if(file__returVecRef_temp.is_open()) {
         Useful::PrintOut("Using returVecRef_temp.csv file.",std::string::npos, "left","",true,false,false,1,1,&terminalCursorPos);
-        in_stream = file__returVecRef_temp;
+        in_stream.swap(file__returVecRef_temp);
     }
     else {
         Useful::PrintOut("Is NOT using returVecRef_temp.csv file.",std::string::npos, "left","",true,false,false,1,1,&terminalCursorPos);
